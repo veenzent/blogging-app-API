39a21e93b9ed3d8e4576a7a7a90e5eba51d34fa0
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2023-10-24 12:56:07 +0100

    added my-blogs endpoint

--- blogging_app/__init__.py
+++ blogging_app/__init__.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from blogging_app.routers.home import home_routes
-from blogging_app.routers.about import about_routes
-from blogging_app.routers.contacts import contacts_routes
 
 app = FastAPI(title="Blog APP", description="Curiosity births Innovations. Discover articles, thoughts, and professionals from authors on any topic")
 
